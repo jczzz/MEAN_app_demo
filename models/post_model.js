859190c9de323fd9e7068dfb0e49d0b410e22713
Jesse-Zhang <sharpshui@hotmail.com>
2018-02-08 17:55:51 -0800

    Update post_model.js

--- models/post_model.js
+++ models/post_model.js
@@ -1,13 +1,13 @@
 // 引用mongoose模组
 var db = require('../db');
-var Schema = db.Schema;
+
 //create a Post model（class）
 // var Post = mongoose.model( model_name, PostPrototype)
-var PostShema = new Schema({
+var PostShema = new db.Schema({
   username: { type: String, required: true },
   body:     { type: String, required: true },
   date:     { type: Date,   required: true, default: Date.now }
 });
 var Post = db.model('Post', PostShema );
-//将model Post 暴露出去
+//暴露Post model
 module.exports = Post;

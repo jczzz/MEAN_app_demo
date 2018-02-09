1509df2411f08909feeaaa9a512ec7621efe057a
Jesse-Zhang <sharpshui@hotmail.com>
2018-02-08 17:57:10 -0800

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 //interact with MongoDB by using the mongoose ODM
 //a light layer on top of Mongo driver
 var mongoose = require('mongoose');
-// declare a database, and make a connection
+//连接一个database地址
 mongoose.connect('mongodb://localhost:27017/social', function () {
   console.log('mongodb connected');
 });
-// 模组暴露=mongoose
+// 暴露mongoose
 module.exports = mongoose;
